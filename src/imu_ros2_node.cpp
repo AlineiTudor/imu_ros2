3cddd92bc52fc547c3a079ca43614c63d5226b6b
Tudor-Alinei-poiana_adi <Tudor.Alinei-poiana@analog.com>
2026-02-06 13:44:44 +0200

    Injected covariance provider inside main ROS2 node

--- src/imu_ros2_node.cpp
+++ src/imu_ros2_node.cpp
@@ -36,6 +36,7 @@
 #include "adi_imu/velangtemp_data_provider.h"
 #include "adi_imu/velangtemp_ros_publisher.h"
 #include "adi_imu/worker_thread.h"
+#include "adi_imu/imu_covariance_factory.h" // New covariance feature
 #include "rclcpp/rclcpp.hpp"
 
 /**
@@ -124,6 +125,21 @@ int main(int argc, char * argv[])
   imu_std_data_provider->setFrameId(frame_id);
   adi_imu::ImuRosPublisherInterface * imu_std_publisher = new adi_imu::ImuRosPublisher(imu_node);
   imu_std_publisher->setMessageProvider(imu_std_data_provider);
+
+  //==============Covariance provider setup================
+  // Create covariance provider from ROS2 parameters.
+  // The factory declares parameters and creates the appropriate provider
+  std::unique_ptr<adi_imu::ImuCovarianceInterface> covariance_provider = 
+    adi_imu::ImuCovarianceFactory::createFromParameters(imu_node);
+  
+  // Inject covariance provider into IMU data provider if enabled
+  if (covariance_provider){
+    imu_std_data_provider->setCovarianceProvider(covariance_provider.get());
+    RCLCPP_INFO(imu_node->get_logger(), "Covariance provider arrached to ImuDataProvider.");
+  } else {
+    RCLCPP_INFO(imu_node->get_logger(), "Covariance computation disabled (covariance.enable = false).");
+  }
+  //=======================================================
 
   adi_imu::VelAngTempDataProviderInterface * vel_ang_data_provider = nullptr;
   adi_imu::VelAngTempRosPublisherInterface * vel_ang_publisher = nullptr;
